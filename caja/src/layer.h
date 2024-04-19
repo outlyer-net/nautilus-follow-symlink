67d63bdf7cd3a907525edc404093cece92aa2837
Toni Corvera <outlyer@gmail.com>
2024-04-19 21:48:15 +0200

    Add missing entries for caja variant

--- caja/src/layer.h
+++ caja/src/layer.h
@@ -14,3 +14,7 @@
 #define nautilus_module_list_types    caja_module_list_types
 #define nautilus_module_shutdown    caja_module_shutdown
 #define NAUTILUS_TYPE_MENU_PROVIDER    CAJA_TYPE_MENU_PROVIDER
+#undef NAUTILUS_COMMAND
+#undef FSL_MSG_PREFIX
+#define NAUTILUS_COMMAND "caja"
+#define FSL_MSG_PREFIX "caja-follow-symlink"
